e293cc927284cd6152db13d681f3fe4e5447453d
MattDahEpic <mattdahepic@gmail.com>
2017-09-11 10:37:43 -0600

    begin recieve room code work

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public static class GameManager {
+    public static WebSocket ws;
+    public static string gameCode = null;
+}
--- Assets/Scripts/LobbyManagement.cs
+++ Assets/Scripts/LobbyManagement.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WebSocketSharp;
 
 public class LobbyManagement : MonoBehaviour {
-
-	// Use this for initialization
+    public GameObject loadScreen;
+    public GameObject lobbyScreen;
 	void Start () {
-		
+        GameManager.ws = new WebSocket("ws://localhost:36245");
+        Debug.Log("Opening connection...");
+        GameManager.ws.OnOpen += (sender, e) => {
+            GameManager.ws.Send("{\"type\":\"game\"}");
+        };
+        GameManager.ws.OnMessage += (sender, e) => {
+            if (GameManager.gameCode == null) { //has no room code
+                //TODO make a Message_RecieveRoomCode out of `e`, set GameManager.gameCode to the code supplied
+            }
+        };
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+        if (GameManager.gameCode == null) return; //show loading screen until we have a room code
+        loadScreen.SetActive(false);
+        lobbyScreen.SetActive(true);
 	}
 }
