fc2491af3d473407ce02149d99f4c5521d365cdb
MattDahEpic <mattdahepic@gmail.com>
2017-09-11 23:00:13 -0600

    uppercasing pt 1

--- _resources/js/main.js
+++ _resources/js/main.js
@@ -2,13 +2,13 @@ window.onload = function () {
     if (window.location.hash != '') {
         document.getElementById('base-gameCode').value = window.location.hash.split('#')[1];
     }
-    history.pushState(null, "Brawl o' Chums", location.href.split("#")[0]);
+    history.pushState(null, "Brawl o' Chums", location.href.split("#")[0].toUpperCase());
 };
 window.onhashchange = function () {
     if (window.location.hash != '') {
         document.getElementById('base-gameCode').value = window.location.hash.split('#')[1];
     }
-    history.pushState(null, "Brawl o' Chums", location.href.split("#")[0]);
+    history.pushState(null, "Brawl o' Chums", location.href.split("#")[0].toUpperCase());
 };
 
 function DoJoinGame () {
