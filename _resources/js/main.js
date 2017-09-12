d6e3ac68e04e628597489509b5d0d2fb7ade58a7
MattDahEpic <mattdahepic@gmail.com>
2017-09-11 20:05:39 -0600

    ensure if people type things into the hash it updates

--- _resources/js/main.js
+++ _resources/js/main.js
@@ -1,8 +1,14 @@
 window.onload = function () {
     if (window.location.hash != '') {
         document.getElementById('base-gameCode').value = window.location.hash.split('#')[1];
-        window.location.hash = "";
     }
+    history.pushState(null, "Brawl o' Chums", location.href.split("#")[0]);
+};
+window.onhashchange = function () {
+    if (window.location.hash != '') {
+        document.getElementById('base-gameCode').value = window.location.hash.split('#')[1];
+    }
+    history.pushState(null, "Brawl o' Chums", location.href.split("#")[0]);
 };
 
 function DoJoinGame () {
