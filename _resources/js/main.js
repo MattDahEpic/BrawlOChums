b39ea9f1e4e6002218c4359a5e47561e6c2cf925
MattDahEpic <mattdahepic@gmail.com>
2017-09-13 08:39:34 -0600

    begin websocket handling

--- _resources/js/main.js
+++ _resources/js/main.js
@@ -11,6 +11,26 @@ window.onhashchange = function () {
     history.pushState(null, "Brawl o' Chums", location.href.split("#")[0].toUpperCase());
 };
 
+//TODO player name control (<=20 chars, emoji+utf8)
+
+var gameCode;
+
 function DoJoinGame () {
-    //TODO
+    gameCode = document.getElementById('base-gameCode').value.toUpperCase();
+    var ws = new WebSocket("ws://localhost:36245");
+    ws.onopen = function (event) {
+        ws.send("{\"type\":\"client\",\"code\":\""+gameCode+"\""); //TODO include name in handshake
+    };
+    ws.onmessage = function (msg) {
+        //TODO if this is a "client added to session" message, set display: none; on the codeEntry div
+        //TODO handle message
+    };
+    ws.onclose = function (evt) {
+        console.log("Connection lost! ("+evt.code+":"+evt.reason+")");
+        if (evt.code == 4001) {
+            //TODO invalid game code
+            return;
+        }
+        //TODO show disconnected dialog
+    };
 }
