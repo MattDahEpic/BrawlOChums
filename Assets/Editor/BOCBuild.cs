2811a33abfb104dc515e5e41a0d759958853e77f
MattDahEpic <mattdahepic@gmail.com>
2017-09-25 14:41:17 -0600

    fix jenkinsfile

---
+++ Assets/Editor/BOCBuild.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BOCBuild {
+    public static void BuildWindows () {
+		System.Console.WriteLine("Beginning Windows Build");
+        string path = System.Environment.GetEnvironmentVariable("WORKSPACE");
+        if (path == null) EditorApplication.Exit(1);
+        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, path + @"\Build\Windows.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+		EditorApplication.Exit(0);
+    }
+}
---
+++ Jenkinsfile
@@ -0,0 +1,15 @@
+pipeline {
+  agent any
+  stages {
+    stage('Build Windows') {
+      steps {
+        bat 'Unity.exe -batchmode -executeMethod BOCBuild.BuildWindows -projectPath "%WORKSPACE%" -logfile'
+      }
+    }
+    stage('Archive Windows') {
+      steps {
+        archiveArtifacts 'Build\\Windows*'
+      }
+    }
+  }
+}
