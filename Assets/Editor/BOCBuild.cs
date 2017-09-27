c3e8867a4cc20ef4508a01f170d2bf240a587c89
MattDahEpic <mattdahepic@gmail.com>
2017-09-27 11:03:03 -0600

    mac and linux builds

--- Assets/Editor/BOCBuild.cs
+++ Assets/Editor/BOCBuild.cs
@@ -11,4 +11,18 @@ public class BOCBuild {
         BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, path + @"\Build\BOC-Windows.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
 		EditorApplication.Exit(0);
     }
+    public static void BuildMac () {
+        System.Console.WriteLine("Beginning Mac Build");
+        string path = System.Environment.GetEnvironmentVariable("WORKSPACE");
+        if (path == null) EditorApplication.Exit(1);
+        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, path + @"\Build\BOC-Mac.app", BuildTarget.StandaloneOSXUniversal, BuildOptions.None);
+        EditorApplication.Exit(0);
+    }
+    public static void BuildLinux () {
+        System.Console.WriteLine("Beginning Linux Build");
+        string path = System.Environment.GetEnvironmentVariable("WORKSPACE");
+        if (path == null) EditorApplication.Exit(1);
+        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, path + @"\Build\BOC-Linux.x86", BuildTarget.StandaloneLinuxUniversal, BuildOptions.None);
+        EditorApplication.Exit(0);
+    }
 }
--- Jenkinsfile
+++ Jenkinsfile
@@ -6,9 +6,21 @@ pipeline {
         bat 'Unity.exe -batchmode -executeMethod BOCBuild.BuildWindows -projectPath "%WORKSPACE%" -logfile'
       }
     }
-    stage('Archive Windows') {
+    stage('Build Mac') {
+      steps {
+        bat 'Unity.exe -batchmode -executeMethod BOCBuild.BuildMac -projectPath "%WORKSPACE%" -logfile'
+      }
+    }
+    stage('Build Linux') {
+      steps {
+        bat 'Unity.exe -batchmode -executeMethod BOCBuild.BuildLinux -projectPath "%WORKSPACE%" -logfile'
+      }
+    }
+    stage('Archive Files') {
       steps {
         archiveArtifacts 'Build\\BOC-Windows*'
+        archiveArtifacts 'Build\\BOC-Mac*'
+        archiveArtifacts 'Build\\BOC-Linux*'
       }
     }
   }
