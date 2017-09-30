6c0ba4cf660628f55f7d731587c6c7a02592cebf
MattDahEpic <mattdahepic@gmail.com>
2017-09-30 12:52:46 -0600

    server forwards client responses to server

--- boc_trivia.js
+++ boc_trivia.js
@@ -1,14 +1,14 @@
 const s = require('./boc_struct.js');
 
 module.exports = function (messageRaw,message, ws) {
-    if (message.trivia === "loadquestions" && ws.clientType === "game") { //recieving questions from game to send to clients
+    //recieving questions from game to send to clients
+    //OR game trigger to display questions on client
+    if ((message.trivia === "loadquestions" || message.trivia === "displayquestion") && ws.clientType === "game") {
         const clients = s.connections.get(ws.gameCode).clients.values();
         for (let i = 0; i < clients.length; i++) {
             clients[i].send(messageRaw);
         }
-    } else if (false) { //TODO game trigger to show question
-
-    } else if (false) { //TODO client sending answers to server
-
+    } else if (message.trivia === "response" && ws.clientType === "client") { //client sending answers to server
+        s.connections.get(ws.gameCode).server.send("{\"trivia\":\"response\",\"identifier\":\""+ws.clientIdentifier+"\",\"selected\":\""+message.selected+"\"}");
     }
 };
