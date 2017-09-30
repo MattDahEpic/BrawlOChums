9b8bae9f4277191ddfa080d77116c83b326c975f
MattDahEpic <mattdahepic@gmail.com>
2017-09-30 12:47:31 -0600

    make client handle trivia!

--- _resources/js/main.js
+++ _resources/js/main.js
@@ -18,9 +18,12 @@ window.onhashchange = function () {
 
 //TODO player name control (<=20 chars, emoji+utf8)
 
+let ws;
 var gameCode;
 var playerName;
 var identifier;
+let questions;
+let currentAnswers;
 
 function DoJoinGame () { //TODO spinny animation to show work
     document.getElementById('base-submit').isDisabled = true;
@@ -35,7 +38,7 @@ function DoJoinGame () { //TODO spinny animation to show work
         return;
     }
     document.cookie = "boc-playername="+playerName+"; expires=33071673599000; path=/"; //set the player name to a cookie for later retrieval
-    var ws = new WebSocket("ws://localhost:36245");
+    ws = new WebSocket("ws://localhost:36245");
     ws.onopen = function (event) {
         console.log("Connection Established!");
         //send type, code, name, and identifier (if we have one) to the server
@@ -50,10 +53,46 @@ function DoJoinGame () { //TODO spinny animation to show work
             identifier = message.identifier;
             document.cookie = "boc-identifier="+message.identifier+"; expires="+now.toUTCString()+"; path=/"; //store the identifier the server gave us for 30 mins
             document.getElementById('base-codeEntry').style.display = "none";
-            document.getElementById('other-lobby').style.display = "initial";
+            document.getElementById('other-logo').style.display = "initial";
             return;
         }
-        //TODO handle message
+        //handle receiving questions
+        if (message.trivia === "loadquestions") {
+            questions = message.questions;
+        }
+        //handle displaying current question
+        if (message.trivia === "displayquestion") {
+            document.getElementById('other-logo').style.display = "none";
+            document.getElementById('trivia').style.display = "initial";
+            let answers;
+            switch (message.display) {
+                case "1":
+                    document.getElementById('trivia-question').innerText = questions.question1.question;
+                    answers = shuffleArray(questions.question1.answers);
+                    break;
+                case "2":
+                    document.getElementById('trivia-question').innerText = questions.question2.question;
+                    answers = shuffleArray(questions.question2.answers);
+                    break;
+                case "3":
+                    document.getElementById('trivia-question').innerText = questions.question3.question;
+                    answers = shuffleArray(questions.question3.answers);
+                    break;
+                case "4":
+                    document.getElementById('trivia-question').innerText = questions.question4.question;
+                    answers = shuffleArray(questions.question4.answers);
+                    break;
+                case "5":
+                    document.getElementById('trivia-question').innerText = questions.question5.question;
+                    answers = shuffleArray(questions.question5.answers);
+                    break;
+            }
+            currentAnswers = answers;
+            document.getElementById('trivia-question1').innerText = answers[0];
+            document.getElementById('trivia-question2').innerText = answers[1];
+            document.getElementById('trivia-question3').innerText = answers[2];
+            document.getElementById('trivia-question4').innerText = answers[3];
+        }
     };
     ws.onclose = function (evt) {
         console.log("Connection lost! ("+evt.code+")");
@@ -68,4 +107,10 @@ function DoJoinGame () { //TODO spinny animation to show work
                 break;
         }
     };
+}
+
+function SelectTriviaAnswer (selectedAnswer) {
+    ws.send("{\"trivia\":\"response\",\"selected\":\""+currentAnswers[selectedAnswer]+"\"}");
+    document.getElementById('other-logo').style.display = "initial";
+    document.getElementById('trivia').style.display = "none";
 }
--- _resources/js/support.js
+++ _resources/js/support.js
@@ -4,3 +4,12 @@ function getCookie(name) {
     if (parts.length == 2) return parts.pop().split(";").shift();
     return null;
 };
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
