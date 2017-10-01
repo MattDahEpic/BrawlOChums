9dd30b675985305975e1797ea6c87c6c2b0dbce4
MattDahEpic <mattdahepic@gmail.com>
2017-10-01 11:39:57 -0600

    forward forced question hiding messages

--- boc_trivia.js
+++ boc_trivia.js
@@ -3,7 +3,7 @@ const s = require('./boc_struct.js');
 module.exports = function (messageRaw,message, ws) {
     //recieving questions from game to send to clients
     //OR game trigger to display questions on client
-    if ((message.trivia === "loadquestions" || message.trivia === "displayquestion") && ws.clientType === "game") {
+    if ((message.trivia === "loadquestions" || message.trivia === "hidequestion" || message.trivia === "displayquestion") && ws.clientType === "game") {
         const clients = s.connections.get(ws.gameCode).clients.values();
         for (let i = 0; i < clients.length; i++) {
             clients[i].send(messageRaw);
