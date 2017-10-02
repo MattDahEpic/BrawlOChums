da7994e6e637c24f5c467775fd61adf167e2502e
MattDahEpic <mattdahepic@gmail.com>
2017-10-01 20:49:07 -0600

    fix crash

--- boc_trivia.js
+++ boc_trivia.js
@@ -6,7 +6,7 @@ module.exports = function (messageRaw,message, ws) {
     if ((message.trivia === "loadquestions" || message.trivia === "hidequestion" || message.trivia === "displayquestion") && ws.clientType === "game") {
         const clients = s.connections.get(ws.gameCode).clients.values();
         for (let i = 0; i < clients.length; i++) {
-            clients[i].send(messageRaw);
+            if (clients[i] !== null) clients[i].send(messageRaw);
         }
     } else if (message.trivia === "response" && ws.clientType === "client") { //client sending answers to server
         s.connections.get(ws.gameCode).server.send("{\"trivia\":\"response\",\"identifier\":\""+ws.clientIdentifier+"\",\"selected\":\""+message.selected+"\"}");
